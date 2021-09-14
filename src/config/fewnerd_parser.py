d76612677ca3149f638a7ce72c49f9b899136dea
Mahak <agarwalmahak13@gmail.com>
2021-09-14 11:29:08 -0700

    fewnerd iob conversion

--- src/config/fewnerd_parser.py
+++ src/config/fewnerd_parser.py
@@ -1,7 +1,5 @@
-import random
 import argparse
-import numpy
-random.seed(1337)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--inp_data', type=str, default='dataset/fewnerd/supervised/dev.txt', help="text file - dataset")
@@ -33,11 +31,11 @@ for i in range(len(lines)):
             new_label='O'
             inside = False
         elif inside == False or i==0:
-            # temp = label
+        
             new_label = 'B-'+label
             inside = True
         elif inside == True and label==lines[i-1].strip().split()[-1]:
-            # print(temp)
+        
             new_label = 'I-'+label
         sent.append([word,new_label])
 
@@ -49,20 +47,3 @@ with open(out_data,'w',encoding='utf8') as f:
             
         f.write('\n')
 
-# print(arr)
-
-
-    # inside = False
-    # label = line.strip().split()[-1]
-
-    # words = [t["text"] for t in sent["tokens"]]
-    # labels = ['O'] * len(words)
-    # for e in sent["basic_events"]:
-    #      event_type = e["event_type"]
-    #      for span in e["anchors"]["spans"]:
-    #          start = span["grounded_span"]["full_span"]["start_token"]
-    #          end = span["grounded_span"]["full_span"]["end_token"] + 1
-    #          labels[start] = f'B-{event_type}'
-    #          for idx in range(start + 1, end):
-    #              labels[idx] = f'I-{event_type}'
-    # return words, labels
