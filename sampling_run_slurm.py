11e52f93e1f68cbca8721c0476dcadf646621ecc
Kangmin <kangmint@usc.edu>
2021-11-06 22:37:24 +0000

    fix sampling_run_slurm.py to support promot/template=None

--- sampling_run_slurm.py
+++ sampling_run_slurm.py
@@ -2,45 +2,61 @@ import os
 import re
 import argparse
 import numpy
+import time
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--train_file', type=str, required=True, help='Finetuning file')
 parser.add_argument('--dataset', type=str, required=True, help='Dataset')
 parser.add_argument('--data_dir', type=str, required=True, help='Data Directory')
-parser.add_argument('--suffix', type=str, required=True, help='Data Directory')
-parser.add_argument('--prompt', type=str, required=True, help='Data Directory')
-parser.add_argument('--template', type=str, required=True, help='Data Directory')
+parser.add_argument('--suffix', type=str, required=True, help='Size of training file')
+parser.add_argument('--prompt', type=str, required=False, help='Selection strategy')
+parser.add_argument('--template', type=str, required=False, help='Template')
+parser.add_argument('--no_subsamples', type=bool, required=False, default=False, help='No seeded subsamples is this is specified (any value will evaluate to true)')
 
 args = parser.parse_known_args()[0]
 
-suffices = [args.suffix + "_9999", args.suffix + "_1337", args.suffix + "_2021", args.suffix + "_5555", args.suffix + "_42"]
+suffices = ([args.suffix + "_1337", args.suffix + "_2021", args.suffix + "_5555", args.suffix + "_42", args.suffix] if not args.no_subsamples else [args.suffix])
 seeds = ['42', '1337', '2021']
+print(f"\nUsing suffices: {suffices}")
+sys.stdout.flush()
 
 try:
     os.makedirs("logs/" + args.dataset + "/")
 except:
     pass
 
+
+def gen_command_logfilename_modelfolder(args, seed, suffix):
+    """
+        Returns (command, log file, model folder)
+    """
+    log_file = "logs/" + args.dataset + "/" + args.train_file.split('.')[0] + "_" + (args.prompt if args.prompt else "None") + "_" + (args.template if args.prompt else "None") + "_" + suffix + "_" + seed + ".txt"
+    model_folder = "models/" + args.dataset + "/" + args.train_file.split('.')[0] + "_" + (args.prompt if args.prompt else "None") +"_" + (args.template if args.prompt else "None") +  "_"  + suffix + "_" + seed
+    predict_cmd = None
+    predict_cmd = \
+        " python3 " + args.train_file + \
+        " --dataset " + args.dataset + \
+        " --data_dir " + args.data_dir + \
+        " --model_folder " + model_folder + \
+        " --device cuda:0" + \
+        " --percent_filename_suffix " + suffix + \
+        " --num_epochs 50" + \
+        ((" --prompt " + args.prompt + " --template " + args.template) if args.prompt else "") + \
+        " --seed " + seed + " > " + log_file
+
+    return predict_cmd,log_file,model_folder
+
+
 log_files = []
 model_folders = []
 for suffix in suffices:
     for seed in seeds:
-        log_file = "logs/" + args.dataset + "/" + args.train_file.split('.')[0] + "_" + args.prompt + "_" + args.template + "_" + suffix + "_" + seed + ".txt"
-        model_folder = "models/" + args.dataset + "/" + args.train_file.split('.')[0] + "_" + args.prompt + "_" + args.template + "_"  + suffix + "_" + seed
-        predict_cmd = \
-            " python3 " + args.train_file + \
-            " --dataset " + args.dataset + \
-            " --data_dir " + args.data_dir + \
-            " --model_folder " + model_folder + \
-            " --device cuda:0" + \
-            " --percent_filename_suffix " + suffix + \
-            " --num_epochs 50" + \
-            " --prompt " + args.prompt + \
-            " --template " + args.template + \
-            " --seed " + seed + " > " + log_file
+        predict_cmd,log_file,model_folder = gen_command_logfilename_modelfolder(args, seed, suffix)
         log_files.append(log_file)
         model_folders.append(model_folder)
-        print(predict_cmd, "\n")
+        print("Executing command: " + predict_cmd + "\n")
+        sys.stdout.flush() # Otherwise the command won't show for some reason
         os.system(predict_cmd)
 
 for model_folder in model_folders:
@@ -62,6 +78,7 @@ std = numpy.std(arr, axis=0)
 print("average: ", numpy.mean(arr, axis=0))
 print("std: ", numpy.std(arr, axis=0))
 
-with open(args.dataset + "_" + args.train_file.split('.')[0] + "_" + args.prompt + "_" + args.template + "_" + args.suffix + ".txt", 'w') as file:
+# Write result summary to a txt file. 
+with open(args.dataset + "_" + args.train_file.split('.')[0] + "_" + (args.prompt if args.prompt else "None") + "_" + (args.template if args.prompt else "None") + "_" + args.suffix + ".txt", 'w') as file:
     file.write("average: " + str(mean))
     file.write("std: " + str(std))
